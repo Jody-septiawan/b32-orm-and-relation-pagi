4b564c6ba59a3779a07b602373aa26aafe089c9e
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-05 10:13:28 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,99 +1,116 @@
-const { user } = require('../../models')
+const { user } = require('../../models');
 
 exports.addUsers = async (req, res) => {
-    try {
+  try {
+    await user.create(req.body);
 
-        await user.create(req.body)
-
-        res.send({
-            status: 'success',
-            message: 'Add user finished'
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: 'success',
+      message: 'Add user finished',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.getUsers = async (req, res) => {
-    try {
+  try {
+    const users = await user.findAll({
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
 
-        const users = await user.findAll({
-            attributes: {
-                exclude: ['password','createdAt', 'updatedAt']
-            }
-        })
-
-        res.send({
-            status: 'success',
-            data: {
-                users
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: 'success',
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.getUser = async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params;
 
-        const data = await user.findAll({
-            where: {
-                id
-            },
-            attributes: {
-                exclude: ['password', 'createdAt', 'updatedAt']
-            }
-        })
+    const data = await user.findAll({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
 
-        res.send({
-            status: 'success',
-            data: {
-                user: data
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: 'success',
+      data: {
+        user: data,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.updateUser = async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params;
 
-        await user.update(req.body, {
-            where: {
-                id
-            }
-        })
+    await user.update(req.body, {
+      where: {
+        id,
+      },
+    });
 
-        res.send({
-            status: 'success',
-            message: `Update user id: ${id} finished`,
-            data: req.body
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: 'success',
+      message: `Update user id: ${id} finished`,
+      data: req.body,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.deleteUser = async (req, res) => {
-    // code here
-}
+  try {
+    const id = req.params.id;
+
+    await user.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      status: 'success',
+      message: `Delete user with ID: ${id} finished`,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,23 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
-// import controller function here 
-const { addUsers, getUsers, getUser, updateUser } = require('../controllers/user')
+// import controller function here
+const {
+  addUsers,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/user');
 
 // Route
-router.post('/user', addUsers)
-router.get('/users', getUsers)
-router.get('/user/:id', getUser)
-router.patch('/user/:id', updateUser)
+router.post('/user', addUsers);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.patch('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 // add route here
 
-module.exports = router
+module.exports = router;
