20b07f65fd5e0bd9beb54dacb491576b55bb13b0
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-05 09:44:35 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,18 @@
 // import model here
+const { user } = require('../../models');
 
 exports.addUsers = async (req, res) => {
-    // code here
-}
+  try {
+    await user.create(req.body);
+
+    res.send({
+      message: 'Insert data user with ORM finished',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller
 // import controller here
+const { addUsers } = require('../controllers/user');
 
 // Route
 // add route here
+router.post('/user', addUsers);
 
-
-module.exports = router
+module.exports = router;
